e082ffafa28c77036c07e6a6fb21479ebb8b08b2
Suraj Sisodia <59198883+surajsisodia@users.noreply.github.com>
2020-07-11 02:50:06 +0530

    First Upload

---
+++ src/main/java/com/suraj/loginpage/HomeScreen.java
@@ -0,0 +1,35 @@
+package com.suraj.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeScreen extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        btnLogout = findViewById(R.id.logOut);
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeScreen.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
